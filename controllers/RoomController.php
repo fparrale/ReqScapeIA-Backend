494948f5b5a75c3be90a2cad02d3898ac53120fc
FbrzRM <framosmesias@gmail.com>
2024-11-07 08:50:17 -0500

    fix: :zap: Unnecessary clause.

--- controllers/RoomController.php
+++ controllers/RoomController.php
@@ -12,64 +12,54 @@ class RoomController{
         $data = json_decode(file_get_contents('php://input'), true);
         $isAdmin = $this->getUserFromDB($email);
 
-        switch ($isAdmin) {
-            case true: // TODO: TRUE
-                $room_code = $data['room_code'] ?? null;
-                $room_name = $data['room_name'] ?? null;
-                $max_attempts = $data['max_attempts'] ?? null;
+        if($isAdmin){
+            $room_code = $data['room_code'] ?? null;
+            $room_name = $data['room_name'] ?? null;
+            $max_attempts = $data['max_attempts'] ?? null;
 
-                if( empty($room_name)) {
-                    http_response_code(400);
-                    echo json_encode(['message' => 'Nombre de sala es requerido.']);
-                    return;
-                }else if(empty ($room_code)){
-                    http_response_code(400);
-                    echo json_encode(['message' => 'Código de sala es requerido.']);
-                    return;
-                }
+            if( empty($room_name)) {
+                http_response_code(400);
+                echo json_encode(['message' => 'Nombre de sala es requerido.']);
+                return;
+            }else if(empty ($room_code)){
+                http_response_code(400);
+                echo json_encode(['message' => 'Código de sala es requerido.']);
+                return;
+            }
 
-                $room_exists = $this->getRoomFromDB($room_name, $room_code);
-                if ($room_exists) {
-                    http_response_code(400);
-                    echo json_encode(['message' => $room_exists]);
-                    return;
+            $room_exists = $this->getRoomFromDB($room_name, $room_code);
+            if ($room_exists) {
+                http_response_code(400);
+                echo json_encode(['message' => $room_exists]);
+                return;
+            } else {
+                if (is_null($max_attempts)) {
+                    $query = "INSERT INTO rooms (room_code, room_name, user_id) VALUES (:room_code, :room_name, :user_id)";
                 } else {
-                    if (is_null($max_attempts)) {
-                        $query = "INSERT INTO rooms (room_code, room_name, user_id) VALUES (:room_code, :room_name, :user_id)";
-                    } else {
-                        $query = "INSERT INTO rooms (room_code, room_name, user_id, max_attempts) VALUES (:room_code, :room_name, :user_id, :max_attempts)";
-                    }
-                    $stmt = $this->db->prepare($query);
-                    $stmt->bindParam(':room_code', $room_code);
-                    $stmt->bindParam(':room_name', $room_name);
-                    $stmt->bindParam(':user_id', $id);
-                    if (!is_null($max_attempts)) {
-                        $stmt->bindParam(':max_attempts', $max_attempts);
-                    }
-
-                    if ($stmt->execute()) {
-                        http_response_code(201);
-                        echo json_encode(['message' => 'Se ha creado la sala.']);
-                        return true;
-                    } else {
-                        http_response_code(500);
-                        echo json_encode(['message' => 'Error al crear la sala.']);
-                        return false;
-                    }
+                    $query = "INSERT INTO rooms (room_code, room_name, user_id, max_attempts) VALUES (:room_code, :room_name, :user_id, :max_attempts)";
                 }
-            break;
+                $stmt = $this->db->prepare($query);
+                $stmt->bindParam(':room_code', $room_code);
+                $stmt->bindParam(':room_name', $room_name);
+                $stmt->bindParam(':user_id', $id);
+                if (!is_null($max_attempts)) {
+                    $stmt->bindParam(':max_attempts', $max_attempts);
+                }
 
-            case false: //TODO: FALSE
-                http_response_code(400);
-                echo json_encode(['message' => 'Acceso denegado. Solo los administradores pueden crear salas.']);
-                return;
-            break;
-
-            default:
-                http_response_code(400);
-                echo json_encode(['message' => 'Usuario no encontrado, imposible realizar esta acción']);
-                return;
-            break;
+                if ($stmt->execute()) {
+                    http_response_code(201);
+                    echo json_encode(['message' => 'Se ha creado la sala.']);
+                    return true;
+                } else {
+                    http_response_code(500);
+                    echo json_encode(['message' => 'Error al crear la sala.']);
+                    return false;
+                }
+            }
+        }else{
+            http_response_code(400);
+            echo json_encode(['message' => 'Acceso denegado. Solo los administradores pueden crear salas.']);
+            return;
         }
     }
 
